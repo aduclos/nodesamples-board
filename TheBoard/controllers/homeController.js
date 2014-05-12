3e761581c28fca957c03ec941242222e07bc510a
Andrew Duclos <andrew.duclos@gmail.com>
2014-05-11 20:48:01 -0700

    Initial Checkin for Board Node Project Tutorial

---
+++ TheBoard/controllers/homeController.js
@@ -0,0 +1,18 @@
+﻿(function (homeController) {
+    homeController.init = function (app) {
+        
+        var data = require("../data");
+
+        app.get("/", function( req, res) {
+            
+            data.getNoteCategories(function(err, results) {
+
+                    res.render("index", { title: "The Board", error: err, categories: results });
+                });
+            
+            
+            });
+        
+        };
+
+ })(module.exports);
