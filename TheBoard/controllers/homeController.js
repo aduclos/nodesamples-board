d52c5f2dce6f791e187979b6845fb10876a94eea
Andrew Duclos <andrew.duclos@gmail.com>
2014-05-16 16:25:42 -0700

    Added basic mongodb functionality

--- TheBoard/controllers/homeController.js
+++ TheBoard/controllers/homeController.js
@@ -1,18 +1,33 @@
-﻿(function (homeController) {
-    homeController.init = function (app) {
-        
-        var data = require("../data");
+﻿( function ( homeController ) {
+    homeController.init = function ( app ) {
 
-        app.get("/", function( req, res) {
-            
-            data.getNoteCategories(function(err, results) {
+        var data = require( "../data" );
 
-                    res.render("index", { title: "The Board", error: err, categories: results });
-                });
-            
-            
+        app.get( "/", function ( req, res ) {
+
+            data.getNoteCategories( function ( err, results ) {
+
+                res.render( "index", { title: "The Board", error: err, categories: results });
             });
-        
-        };
 
- })(module.exports);
+
+        });
+
+    
+
+    app.post( "/newCategory", function ( req, res ) {
+        var categoryName = req.body.categoryName;
+
+        data.createNewCategory( categoryName, function ( err ) {
+            if ( err ) {
+                console.log( err );
+                res.redirect( "/" );
+            }
+            else {
+                res.redirect( "/notes/" + categoryName );
+            }
+        });
+    });
+
+    };
+})( module.exports );
--- TheBoard/data/index.js
+++ TheBoard/data/index.js
@@ -1,10 +1,83 @@
-﻿(function(data) {
-    
-    var seedData = require("./seedData");
-    
-    data.getNoteCategories = function(next) {
-        next(null, seedData.initialNotes);
+﻿( function ( data ) {
 
-     };
-        
-})(module.exports);
+    var seedData = require( "./seedData" );
+    var database = require( "./database" );
+
+    data.getNoteCategories = function ( next ) {
+        database.getDb( function ( err, db ) {
+            if ( err ) {
+                next( err, null );
+            }
+            else {
+                db.notes.find().sort(   { name: 1 }).toArray( function ( err, results ) {
+
+                    if ( err ) {
+                        next( err, null );
+                    }
+                    else {
+                        next( null, results );
+                    }
+                });
+            }
+        });
+    };
+
+    data.createNewCategory = function(categoryName, next) {
+        database.getDb( function ( err, db ) {
+            if ( err ) {
+                next( err, null );
+            }
+            else {
+                var cat = {
+                    name: categoryName,
+                    notes: []
+                    };
+
+                db.notes.insert(cat, function(err){
+                    if(err){
+                        next(err);
+                        }
+                    else{
+                        next(null);
+                        }
+                });
+                    }
+            });
+        };
+    //data.getNoteCategories = function ( next ) {
+    //    next( null, seedData.initialNotes );
+    //};
+
+    function seedDatabase() {
+        database.getDb( function ( err, db ) {
+            if ( err ) {
+                console.log( "Failed to seed database: " + err );
+            }
+            else {
+                //test to see if data exists
+                if ( db.notes.count( function ( err, count ) {
+
+                    if ( err ) {
+                        console.log( "Failed to retrieve database count" );
+                    }
+                    else {
+                        if ( count == 0 ) {
+                            console.log( "Seeding the database..." );
+                            seedData.initialNotes.forEach( function ( item ) {
+                                db.notes.insert( item, function ( err ) {__dirname
+                                        if ( err ) console.log( "Failed to insert note into database" );
+                                });
+                            });
+                        } else {
+                            console.log( "Database already seeded." );
+                        }
+                    }
+                }) ) {
+                }
+            }
+        });
+    }
+
+    seedDatabase();
+
+})( module.exports );
